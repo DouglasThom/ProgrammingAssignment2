cbe2cdd339bd0eee537694996eb6c5b6c9e64b81
Douglas Thom <dthom@reliancecomfort.com>
2014-07-20 20:54:31 -0400

    Submission for Programming Assignment 2
    Includes the code for the two functions to create the CacheMatrix object
    and the cache solver.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R
+## created by Douglas Thom
+## This script creates a caching mechanism for the long operation of
+## inverting a matrix.
+## Created for R Programming Course: Programming Assignment 2
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a matrix manager allowing for the caching of
+## a solve'd matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(sourceMatrix = matrix()) {
+        inverseMatrix <- NULL
+        set <- function(setMatrix) {
+                sourceMatrix <<- setMatrix
+                inverseMatrix <<- NULL
+        }
+        get <- function() sourceMatrix
+        setMatrix <- function(newMatrix) inverseMatrix <<- newMatrix
+        getMatrix <- function() inverseMatrix
+        list(set = set, get = get,
+                setMatrix = setMatrix,
+                getMatrix = getMatrix)
 }
 
-
-## Write a short comment describing this function
+getwd
+## cacheSolve enables the matrix manager to generate a solution
+## for a matrix and save it to the cache.
 
 cacheSolve <- function(x, ...) {
+        solvedMatrix <- x$getMatrix()
+        if ( !is.null( solvedMatrix ) ) {
+                message( "Returning cached matrix." )
+                return( solvedMatrix )
+        }
+        message( "Generating new matrix" )
+        matrixData <- x$get()
+        newSolution <- solve( matrixData, ... )
+        x$setMatrix( newSolution )
+        
         ## Return a matrix that is the inverse of 'x'
+        newSolution
 }
